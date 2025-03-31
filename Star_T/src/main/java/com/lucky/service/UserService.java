2a0f8ca7bf8659e8459a50ebe13b52c679808795
Stitch_Mac <lkwyouxiang@126.com>
2025-03-31 17:54:16 +0800

    2025.03.31 Mac

--- Star_T/src/main/java/com/lucky/service/UserService.java
+++ Star_T/src/main/java/com/lucky/service/UserService.java
@@ -1,6 +1,5 @@
 package com.lucky.service;
 
-import com.lucky.entity.UserEntity;
 
 public interface UserService {
     /**
@@ -10,6 +9,4 @@ public interface UserService {
      * @return 验证是否成功
      */
     boolean loginValidate(String email, String password);
-//    int add_user(UserEntity user);
-//    UserEntity selectUserEntityByEmail(String email);
 }
--- Star_T/src/main/java/com/lucky/service/impl/UserServiceImpl.java
+++ Star_T/src/main/java/com/lucky/service/impl/UserServiceImpl.java
@@ -16,14 +16,4 @@ public class UserServiceImpl implements UserService {
     public boolean loginValidate(String email, String password) {
         return "admin@126.com".equals(email) && "123456".equals(password);
     }
-
-    @Override
-    public int add_user(UserEntity user) {
-        return userEntityMapper.addUserEntity(user);
-    }
-
-    @Override
-    public UserEntity selectUserEntityByEmail(String email) {
-        return userEntityMapper.selectUserEntityByEmail(email);
-    }
 }
